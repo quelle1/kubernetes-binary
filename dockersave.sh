f2e7cf2225e39221674ff8b79a7746229bb9729c
quelle1 <iecas_wenkai@yahoo.com>
2021-05-20 14:33:55 +0800

    add arm64

--- dockersave.sh
+++ dockersave.sh
@@ -19,5 +19,7 @@ docker tag quelle/k8s-dns-sidecar:1.15.10 k8s.gcr.io/k8s-dns-sidecar:1.15.10
 
 # images load
 docker save k8s.gcr.io/k8s-dns-kube-dns:1.15.10 k8s.gcr.io/k8s-dns-dnsmasq-nany:1.15.10 k8s.gcr.io/k8s-dns-sidecar:1.15.10 > k8s-dns-1.15.10.tar
-docker save kubernetesui/metrics-scraper:v1.0.4 kubernetesui/dashboard:v2.0.1 > k8s-dashboard-v1.19.0.tar
-docker save k8s.gcr.io/coredns:1.7.0 k8s.gcr.io/etcd:3.4.9-1 k8s.gcr.io/pause:3.2 > k8s-pause-etcd-coredns-v1.19.0.tar
+docker save kubernetesui/metrics-scraper:v1.0.4 kubernetesui/dashboard:v2.0.1 > k8s-dashboard-v1.19.4.tar
+docker save k8s.gcr.io/coredns:1.7.0 k8s.gcr.io/etcd:3.4.9-1 k8s.gcr.io/pause:3.2 > k8s-pause-etcd-coredns-v1.19.4.tar
+
+docker save k8s.gcr.io/kube-apiserver:v1.19.4   k8s.gcr.io/kube-controller-manager:v1.19.4 k8s.gcr.io/kube-scheduler:v1.19.4 k8s.gcr.io/kube-proxy:v1.19.4 k8s.gcr.io/pause:3.2 k8s.gcr.io/etcd:3.4.9-1 k8s.gcr.io/coredns:1.7.0 > k8s-pause-etcd-coredns-v1.19.4.tar
